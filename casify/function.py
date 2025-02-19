b1f070056804dbfcd862d9053a301f6ecca6c442
reneaas <reneaas@uio.no>
2025-02-19 21:43:25 +0100

    update

--- casify/function.py
+++ casify/function.py
@@ -122,7 +122,7 @@ class Function:
             xmin, xmax = (-6, 6)
             ymin, ymax = (-6, 6)
 
-        fn_label = "y = " + sympy.latex(self._f_expr)
+        fn_label = "y = " + sympy.latex(self._f_expr, mul_symbol="dot")
         print(fn_label)
         fn_label = f"${fn_label}$"
         fig, ax = plotmath.plot(
