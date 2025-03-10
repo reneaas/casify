3e6a213ff932ba85ab618fd24198ae9cae0fe9ed
reneaas <reneaas@uio.no>
2025-03-10 23:29:28 +0100

    update

--- casify/triangle.py
+++ casify/triangle.py
@@ -159,7 +159,6 @@ def _draw_angle_arc(
 
     if side_label:
         # Get the two points that form the side opposite to vertex
-        p1, p2 = other_points
 
         # Calculate midpoint of the side
         midpoint = np.array([(p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2])
